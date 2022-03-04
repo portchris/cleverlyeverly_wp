2d7da99323d65a30cad259214ae0c4584ab8b8f5
Chris Rogers <chris@convertdigital.com.au>
2022-03-04 12:50:05 +0000

    feature/v2 - DELETE EVERYTHING and start again.

--- wp-content/plugins/event-list/admin/js/admin_main.js
+++
@@ -1,8 +0,0 @@
-// Javascript functions for event-list admin_main page
-
-// Confirmation for event deletion
-function eventlist_deleteEvent (id) {
-	if (confirm("Are you sure you want to delete this event from you the database? This is a permanent action.")) {
-		document.location.href = "?page=el_admin_main&id=" + id + "&action=delete&noheader=true";
-	}
-}
