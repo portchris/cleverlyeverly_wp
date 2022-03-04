311b258cfbe827bd0f9fd36b92f407930ec15db0
Chris Rogers <chris@convertdigital.com.au>
2022-03-04 13:20:32 +0000

    Begin Docker Compose environment on V2

---
+++ env/nginx/scripts/start.sh
@@ -0,0 +1,36 @@
+# Any extra start up scripts here to run on container instantiation
+#!/bin/bash
+
+ENV="/etc/.env"
+CONF_DEFAULT="/etc/nginx/conf.d/default.conf"
+
+# Update Nginx config depending on environment files
+if [ -f $ENV ]; then
+	export $(grep -v '^#' $ENV | xargs)
+
+	if [ -z ${TEMPLATE+x} ]; then
+		echo "TEMPLATE not set!"
+		exit 1
+	fi
+
+	if [ -f $CONF_DEFAULT ]; then
+		rm $CONF_DEFAULT
+	fi
+
+	# Update CONF to match current domain env
+	if [ ! -f $CONF ]; then
+		CONF="/etc/nginx/conf.d/$TEMPLATE.template"
+	fi
+
+	cp $CONF $CONF_DEFAULT
+	sed -i -e "s/{TEMPLATE}/${TEMPLATE}/g" $CONF_DEFAULT
+	echo # \n
+	cat $CONF_DEFAULT
+	echo # \n
+
+	# Restart web server in Docker mode
+	nginx -g "daemon off;"
+else
+	echo "Error: no /etc/nginx/.env environment variables file found!"
+	exit 1
+fi
